3b3eaa092ff2385b802e67c9cbcf0f197d5e56fd
Hikoyu <azrianpintar@gmail.com>
2023-12-12 08:41:07 -0800

    page 1 mobile beressss

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-slides();
+
 const link = ["asset/des02.jpg", "asset/des03.jpg", "asset/des01.jpg"]
+let counterlik1 = 0;
+let counterlik2 = 1;
+let counterlik3 = 2;
+slides()
 function slides() {
-    
-    setTimeout(slides, 3000);
+    const img1 = document.getElementById("i1");
+    const img2 = document.getElementById("i2");
+    const img3 = document.getElementById("i3");
+    if (counterlik1 == 3){
+        counterlik1 = 0
+    };
+    if (counterlik2 == 3){
+        counterlik2 = 0
+    };
+    if (counterlik3 == 3){
+        counterlik3 = 0
+    };
+    img1.style.backgroundImage = `url(${link[counterlik1]})`;
+    img2.style.backgroundImage = `url(${link[counterlik2]})`;
+    img3.style.backgroundImage = `url(${link[counterlik3]})`;
+    counterlik1++;
+    counterlik2++;
+    counterlik3++; 
+    setTimeout(slides, 4500);
 }
